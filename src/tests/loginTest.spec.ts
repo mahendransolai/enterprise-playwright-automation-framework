505b1bfc1e724eb0f78409076bb923b9b27cbc44
Mahendra Solai <mahendransolai@gmail.com>
2024-01-28 20:37:26 -0500

    Test to validate  gitHub Actions

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -2,27 +2,29 @@
 
 import  {test} from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
+import { decrypt, encrypt } from "../utils/CryptojsUtil";
+
 
 test("test", async ({page}) =>{
 
-    const loginPage = new LoginPage(page);
+    // const loginPage = new LoginPage(page);
 
-    await loginPage.navigateToLoginPage();
-    // await loginPage.fillUsername("mahensphotography@gmail.com");
-    // await loginPage.fillPasswors("Arjun123$");
-    await loginPage.fillUsername(process.env.userid!);
-    await loginPage.fillPassword(process.env.password!);
+    // await loginPage.navigateToLoginPage();
+    // // await loginPage.fillUsername("mahensphotography@gmail.com");
+    // // await loginPage.fillPasswors("Arjun123$");
+    // await loginPage.fillUsername(process.env.userid!);
+    // await loginPage.fillPassword(process.env.password!);
 
 
-    const homepage = await loginPage.clickLoginButton();
-    await homepage.expectServiceTitleToBeVisible();
+    // const homepage = await loginPage.clickLoginButton();
+    // await homepage.expectServiceTitleToBeVisible();
 
 });
 
 test("Sample env test ", async({page}) => {
 
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.userid);
-    console.log(process.env.password);
+    // console.log(process.env.NODE_ENV);
+    // console.log(process.env.userid);
+    // console.log(process.env.password);
 
 })
