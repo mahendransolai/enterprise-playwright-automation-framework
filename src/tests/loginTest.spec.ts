e263ade202177b78d06c4aa342eea6a76b2c7532
Mahendra Solai <mahendransolai@gmail.com>
2024-02-08 09:52:24 -0500

    Auth Login Updated

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,23 +1,24 @@
 //Test Script Using the Page Object Model
 
-import  {test} from "@playwright/test";
+import  {test, expect} from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import { decrypt, encrypt } from "../utils/CryptojsUtil";
 
+const authFile = "src/config/auth.json";
 
 test("test", async ({page}) =>{
 
-    // const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
 
-    // await loginPage.navigateToLoginPage();
-    // // await loginPage.fillUsername("mahensphotography@gmail.com");
-    // // await loginPage.fillPasswors("Arjun123$");
+    await loginPage.navigateToLoginPage();
+    await loginPage.fillUsername("mahens@gmail.com");
+    await loginPage.fillPassword("Arjun123$");
     // await loginPage.fillUsername(process.env.userid!);
     // await loginPage.fillPassword(process.env.password!);
 
-
-    // const homepage = await loginPage.clickLoginButton();
-    // await homepage.expectServiceTitleToBeVisible();
+    const homepage = await loginPage.clickLoginButton();
+    await homepage.expectServiceTitleToBeVisible();
+    await page.context().storageState({path: authFile})
 
 });
 
@@ -26,5 +27,16 @@ test("Sample env test ", async({page}) => {
     // console.log(process.env.NODE_ENV);
     // console.log(process.env.userid);
     // console.log(process.env.password);
+
+})
+
+test.only("Using Auth File ", async({browser}) => {
+
+    const context = await browser.newContext({storageState: authFile});
+    const page =  await context.newPage();
+    await page.goto("https://mahensphotography-dev-ed.develop.lightning.force.com/lightning/setup/SetupOneHome/home");
+    await expect(page.getByRole("link",{ name: "Accounts"})).toBeVisible();
+    await expect(page.getByRole("link",{ name: "Accounts"})).toBeVisible();
+    
 
 })
